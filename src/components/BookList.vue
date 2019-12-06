fcc2e451b32d0c6b7938f551228320c2508b9272
zmiles17 <zgm_striker17@hotmail.com>
2019-12-06 17:38:17 -0500

    m3

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,8 +1,16 @@
 <template>
   <div>
     <h1>{{title}}</h1>
-    <ul>
+    <ul class="exists">
       <book-item v-for='book in books' :key='book.id' :book='book'></book-item>
+    </ul>
+    <hr>
+    <h2>Filtered Books by Ownership</h2>
+    <select v-model="holding">
+      <option v-for="filter in filters">{{filter}}</option>
+    </select>
+    <ul class="exists">
+      <book-item v-for='book in filteredBooks' :key='book.id' :book='book'></book-item>
     </ul>
     <br><hr>
     <book-form @addBook='appendBook'></book-form>
@@ -10,6 +18,7 @@
 </template>
 
 <script>
+import _ from 'lodash';
 import BookItem from "./BookItem";
 import BookForm from "./BookForm";
 
@@ -19,10 +28,12 @@ export default {
     return {
       title: "All Books",
       states: ["Want to Read", "Read", "Reading"],
+      filters: ["bought", "borrowed"],
+      holding: "bought",
       books: [
-        { title: "Self-Reliance", author: "Ralph Waldo Emerson", finishedReading: true },
-        { title: "American Gods", author: "Neil Gaiman", finishedReading: false },
-        { title: "Amusing Ourselves to Death", author: "Neil Postman", finishedReading: true }
+        { title: "Self-Reliance", author: "Ralph Waldo Emerson", finishedReading: true, ownership: "borrowed" },
+        { title: "American Gods", author: "Neil Gaiman", finishedReading: false, ownership: "bought" },
+        { title: "Amusing Ourselves to Death", author: "Neil Postman", finishedReading: true, ownership: "borrowed" }
       ]
     };
   },
@@ -30,9 +41,19 @@ export default {
     BookItem,
     BookForm
   },
+  computed: {
+    filteredBooks() {
+      return _.filter(this.books, ["ownership", this.holding]);
+    }
+  },
   methods: {
     appendBook(bookData) {
-      this.books.push({ title: bookData.bookTitle, author: bookData.bookAuthor, finishedReading: bookData.finishedReading });
+      this.books.push({ 
+        title: bookData.bookTitle,
+        author: bookData.bookAuthor,
+        finishedReading: bookData.finishedReading,
+        ownership: bookData.ownership 
+      });
     }
   }
 };
